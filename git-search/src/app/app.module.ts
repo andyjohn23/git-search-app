52d4181873b5b1abc8d026fbea517889caa3682c
andyjohn23 <andrewnick906@gmail.com>
2020-10-04 16:39:47 +0300

    updated files

--- git-search/src/app/app.module.ts
+++ git-search/src/app/app.module.ts
@@ -7,12 +7,14 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { UserProfileComponent } from './components/user-profile/user-profile.component';
 import { UserService } from './service/user.service';
 import { HttpClientModule } from '@angular/common/http';
+import { RepositoriesComponent } from './components/repositories/repositories.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    UserProfileComponent
+    UserProfileComponent,
+    RepositoriesComponent
   ],
   imports: [
     BrowserModule,
